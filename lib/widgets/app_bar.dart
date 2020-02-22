1a55e4f687752e0d3fed5a96f63985e6e9a7cbc3
ramyak-mehra <rmehra_be19@thapar.edu>
2020-02-22 10:19:17 -0800

    Custom App Bar

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: AssetImage('lib/assets/dsc_logo.png'),
+              height: MediaQuery.of(context).size.height * 0.0345394736830744,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'DSC TIET',
+              style: GoogleFonts.poppins(fontSize: 25),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image(
+              image: AssetImage('lib/assets/menu_icon.png'),
+              height: MediaQuery.of(context).size.height * 0.0345394736830744,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
