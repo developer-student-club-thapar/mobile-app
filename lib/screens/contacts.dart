2f7b6ef01f27a44ccab102ba9eba7a520cd50873
Eufrasia <prashantshaan98@gmail.com>
2020-03-30 02:26:32 +0530

    Add Join us on slack widget

--- lib/screens/contacts.dart
+++ lib/screens/contacts.dart
@@ -2,7 +2,7 @@ import 'package:dsc_app/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:dsc_app/widgets/app_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:dsc_app/models/contact.dart';
 
 class Contacts extends StatefulWidget {
   @override
@@ -14,107 +14,13 @@ class _ContactsState extends State<Contacts> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: 'DSC TIET'),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 25,
-            ),
-            //image
-            Center(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-                child: Image(
-                  image:
-                      AssetImage('lib/assets/undraw_dev_productivity_umsq.png'),
-                ),
-              ),
-            ),
-            //Text
-            Center(
-              child: Container(
-                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                child: Text(
-                  "At DSC TIET, our aim is to spread awareness and teach. Developers, come together under one roof to create a community which inspires thousands. Join Us!",
-                  style: GoogleFonts.poppins(
-                    color: Color(0xFF7C6C72),
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ),
-            //Twin Buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(
-                  onPressed: _launchCoD_URL,
-                  color: redColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(30.0),
-                    side: BorderSide(color: redColor),
-                  ),
-                  splashColor: Colors.redAccent[400],
-                  textColor: Colors.white,
-                  disabledColor: Colors.grey,
-                  disabledTextColor: Colors.black,
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
-                  child: Text(
-                    "Code of Conduct",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                OutlineButton(
-                  onPressed: _launchSlack_URL,
-                  borderSide: BorderSide(color: redColor, width: 2),
-                  shape: StadiumBorder(),
-                  splashColor: Colors.redAccent[400],
-                  highlightedBorderColor: redColor,
-                  textColor: redColor,
-                  disabledTextColor: Colors.black,
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
-                  child: Text(
-                    "Join our Slack!",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            //Contact form
-            //contact text
-            Center(
-              child: Container(
-                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                child: Text(
-                  "Connect With Us!",
-                  style: GoogleFonts.poppins(
-                    color: Color(0xFF676C72),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-            ),
-            //contact list
-            // name, email, message
-            MyContactForm(),
-          ],
-        ),
-      ),
+      body: MyContactForm(),
     );
   }
 }
 
 class MyContactForm extends StatefulWidget {
+  MyContactForm({Key key}) : super(key: key);
   @override
   _MyContactFormState createState() => _MyContactFormState();
 }
@@ -124,112 +30,164 @@ class _MyContactFormState extends State<MyContactForm> {
   String _name = '';
   String _email = '';
   String _message = '';
+  Future<ContactDetails> _futureContactDetails;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+    return (_futureContactDetails == null)
+        ? Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  //Contact form
+                  //contact text
+                  Center(
+                    child: Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                      child: Text(
+                        "Connect With Us!",
+                        style: GoogleFonts.poppins(
+                          color: Color(0xFF676C72),
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //Name
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Name',
+                      hintText: 'Name',
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      setState(() {
+                        _name = value;
+                      });
+                    },
+                  ),
 
-            //Name
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Name',
-                hintText: 'Name',
+                  //Email
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter Email',
+                      hintText: 'Email',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: validateEmail,
+                    onSaved: (value) {
+                      setState(() {
+                        _email = value;
+                      });
+                    },
+                  ),
+
+                  //Message
+                  Container(
+                    child: TextFormField(
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        labelText: 'Enter Message',
+                        hintText: 'Message',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Enter Your Message';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        setState(() {
+                          _message = value;
+                        });
+                      },
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                          _formKey.currentState.reset();
+                          setState(() {
+                            _futureContactDetails =
+                                createContactDetails(_name, _email, _message);
+                          });
+                        }
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ),
+                ],
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter your name';
-                }
-                return null;
-              },
-              onSaved: (value) {
-                setState(() {
-                  _name = value;
-                });
-              },
             ),
+          )
+        : FutureBuilder<ContactDetails>(
+            future: _futureContactDetails,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
+                      side: BorderSide(color: Colors.lightBlueAccent),
+                    ),
+                    color: Colors.lightBlueAccent,
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/contacts');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        'Successfully Sent! \nSend Another one ?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
+                      side: BorderSide(color: Colors.redAccent),
+                    ),
+                    color: Colors.redAccent,
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/contacts');
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Error! \n Retry',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }
 
-            //Email
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter Email',
-                hintText: 'Email',
-              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: validateEmail,
-              onSaved: (value) {
-                setState(() {
-                  _email = value;
-                });
-              },
-            ),
-
-            //Message 
-            Container(
-              child: TextFormField(
-                maxLines: 5,
-                decoration: InputDecoration(
-                  labelText: 'Enter Message',
-                  hintText: 'Message',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please Enter Your Message';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  setState(() {
-                    _message = value;
-                  });
-                },
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15.0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-
-                    // TODO - Connect with back end 
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Form Submitted')));
-                  }
-                },
-                child: Text('Submit'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-_launchCoD_URL() async {
-  const url =
-      'https://drive.google.com/file/d/1WtxlBDqnZUh7Os1pSwjvv0WDiaYljB-e/view';
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
-
-_launchSlack_URL() async {
-  const url =
-      'https://dscthapar-gspatiala.slack.com/join/shared_invite/enQtNzU2MzA2MjcxNzkyLTkwNDRiNWMzYjUzYjNjYjM0M2JhMDgwOTI3MGQwYWU1NzNlNGMxZGVhNzk0MGZiYTI5YzgwZDhiMTk1MjE4M2M';
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+              return CircularProgressIndicator();
+            },
+          );
   }
 }
 
