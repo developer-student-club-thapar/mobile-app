fc50d5f1a137752ed03499d3d79f5ed990e0e41c
Eufrasia <prashantshaan98@gmail.com>
2020-04-06 17:36:35 +0530

    add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,21 @@
+class User {
+
+  final String uid;
+  
+  User({ this.uid });
+
+}
+
+class UserData {
+
+  final String uid;
+  final String name;
+  final String email;
+  final String gender;
+  final int year;
+  final bool isThaparStudent;
+  //social media links.... 
+
+  UserData({ this.uid, this.name, this.email, this.gender, this.year, this.isThaparStudent});
+
+}
